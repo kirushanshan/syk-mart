da8ea6d95ca7695b4b8965552241e2b1fa0c2e2c
kirushanshan <kirushan93@gmail.com>
2023-04-21 10:47:31 +0530

    react front end page and server setup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,26 @@
+import {Container} from 'react-bootstrap'
+import { BrowserRouter as Router ,Route, Switch , Redirect, Routes} from 'react-router-dom'
+import Header from "./commponents/Header";
+import Footer from "./commponents/Footer"; 
+import HomePage from './pages/HomePage';
+import ProductPage from './pages/ProductPage';
+
 
 function App() {
   return (
-    <>
-      <h1>SYK-mart</h1>
-    </>
+      <Router>
+      <Header/>
+      <main className='py-3'>
+        <Container>
+          <Routes>
+            <Route path='/' Component={HomePage} exact/>
+            <Route path='/product/:id' Component={ProductPage} />
+          </Routes>
+        </Container>
+      </main>
+      <Footer/>
+      </Router>
+
   );
 }
 
