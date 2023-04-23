ba617644700cb3e363ddff1c2f46f12db6c87ed1
kirushanshan <kirushan93@gmail.com>
2023-04-23 09:25:35 +0530

    setup middleware and implement notfound and error handler

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,10 @@
 import express from 'express'
 import dotenv from 'dotenv'
 import colors from 'colors'
+import { notFound, errorHandler } from './middleware/errorMiddleware.js'
 import connectDB from './config/db.js'
 import productRoutes from './routes/productRoutes.js'
+
 
 
 
@@ -23,20 +25,8 @@ app.get('/', (req,res) => {
 
 app.use('/api/products', productRoutes)
 
-app.use((req, res, next) => {
-    const error = new Error (`NOT FOUND - ${req.originalUrl}`)
-    res.status(404)
-    next(error)
-})
-
-app.use((err, req, res, next) => {
-    const statusCode = res.statusCode === 200 ? 500 : res.statusCode
-    res.status(statusCode)
-    res.json({
-        message: err.message,
-        stack: process.env.NODE_ENV === 'production' ? null : err.stack,
-    })
-})
+app.use(notFound)
+app.use(errorHandler)
 
 const PORT = process.env.PORT | 5000
 
