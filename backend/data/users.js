336f4fca1536a9040ecdef0dd36c22c9d0a178f1
kirushanshan <kirushan93@gmail.com>
2023-04-23 09:17:38 +0530

    seeders, routes, models are created and done database implementation

---
+++ backend/data/users.js
@@ -0,0 +1,25 @@
+import bcrypt from 'bcryptjs'
+
+const Users = [
+    {
+        name: 'Admin User',
+        email: 'admin@example.com',
+        password: bcrypt.hashSync('123456', 10), 
+        isAdmin: true
+    },
+    {
+        name: 'John Doe',
+        email: 'john@example.com',
+        password: bcrypt.hashSync('123456', 10), 
+    
+    },
+    {
+        name: 'Jane Doe',
+        email: 'jane@example.com',
+        password:bcrypt.hashSync('123456', 10),  
+
+    
+    },
+]
+
+export default Users
---
+++ backend/routes/productRoutes.js
@@ -0,0 +1,32 @@
+import express from "express"
+const router = express.Router()
+
+import Product from '../models/productModel.js'
+import asyncHandler from 'express-async-handler'
+
+
+
+// @desc Fetch all products
+// @route Get /api/products
+// @access Fetch public
+router.get('/', asyncHandler( async (req,res) => {
+    const products = await Product.find({})
+
+    res.json(products)
+}))
+
+// @desc Fetch Single  products by id
+// @route Get /api/products/:id
+// @access Fetch public
+router.get('/:id',asyncHandler( async (req,res) => {
+    const product = await Product.findById(req.params.id)
+
+    if(product) {
+        res.json(product)
+    } 
+    else {
+        res.status(404).json({message: 'Product not found'})
+    }
+}))
+
+export default router
