d6bc1e3e13779dabd20acee75bcbcf8c58380bce
kirushanshan <kirushan93@gmail.com>
2023-04-23 21:16:46 +0530

    redux install and implement reducer and action dispatch

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -11,7 +11,6 @@ import asyncHandler from 'express-async-handler'
 // @access Fetch public
 router.get('/', asyncHandler( async (req,res) => {
     const products = await Product.find({})
-
     res.json(products)
 }))
 
