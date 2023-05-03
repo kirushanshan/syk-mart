0b7ccfa4b3fd1770ca3744adc717181e129a4cc0
kirushanshan <kirushan93@gmail.com>
2023-05-03 09:23:42 +0530

    addOrder items backend code done

--- backend/server.js
+++ backend/server.js
@@ -5,9 +5,7 @@ import { notFound, errorHandler } from './middleware/errorMiddleware.js'
 import connectDB from './config/db.js'
 import productRoutes from './routes/productRoutes.js'
 import userRoutes from './routes/userRoutes.js'
-
-
-
+import orderRoutes from './routes/orderRoutes.js'
 
 dotenv.config()
 
@@ -22,16 +20,23 @@ app.use(express.json())
 //     next()
 // })
 
-app.get('/', (req,res) => {
-    res.send('API is running....')
+app.get('/', (req, res) => {
+  res.send('API is running....')
 })
 
 app.use('/api/products', productRoutes)
 app.use('/api/users', userRoutes)
+app.use('/api/orders', orderRoutes)
 
 app.use(notFound)
 app.use(errorHandler)
 
 const PORT = process.env.PORT | 5000
 
-app.listen(PORT, console.log(`Server runing in ${process.env.NODE_ENV }  mode on port ${PORT}`.bgYellow.red.bold))
+app.listen(
+  PORT,
+  console.log(
+    `Server runing in ${process.env.NODE_ENV}  mode on port ${PORT}`.bgYellow
+      .red.bold
+  )
+)
