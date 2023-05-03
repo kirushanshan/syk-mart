b249397eecb384770a35316b82eb528158a70e0b
kirushanshan <kirushan93@gmail.com>
2023-05-03 12:05:31 +0530

    updated to paid backend api end point code done

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -1,9 +1,14 @@
 import express from 'express'
 const router = express.Router()
-import { addOrderItems, getOrderById } from '../controllers/orderControl.js'
+import {
+  addOrderItems,
+  getOrderById,
+  updateOrderToPaid,
+} from '../controllers/orderControl.js'
 import { protect } from '../middleware/authMiddleware.js'
 
 router.route('/').post(protect, addOrderItems)
 router.route('/:id').get(protect, getOrderById)
+router.route('/:id/pay').put(protect, updateOrderToPaid)
 
 export default router
